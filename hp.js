344e3cd3b89a6cf63557e72a2b2a5f7a504edca2
Iljawie <ilja.wie@gmail.com>
2020-02-21 06:36:11 +0100

    Do push
    nieuw

--- hp.js
+++ hp.js
@@ -1,8 +1,21 @@
-console.log ("js connected");
+console.log("js connected");
 
-$("#menu-toggle").click(function (e) {
+$("#menu-toggle").click(function(e) {
     e.preventDefault();
     $("#wrapper").toggleClass("toggled");
-  });
+});
 
-  
+// camera Script//
+
+$(function() {
+    var selectedClass = "";
+    $(".filter").click(function() {
+        selectedClass = $(this).attr("data-rel");
+        $("#gallery").fadeTo(100, 0.1);
+        $("#gallery div").not("." + selectedClass).fadeOut().removeClass('animation');
+        setTimeout(function() {
+            $("." + selectedClass).fadeIn().addClass('animation');
+            $("#gallery").fadeTo(300, 1);
+        }, 300);
+    });
+});
